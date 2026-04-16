6f371fff17988b28c0ca625aa8964f7cf62a5265
ssvbava <bandarupalli.kiran@gmail.com>
2026-04-16 11:46:36 +0300

    Initial commit: Base application files

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.json" }],
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+};
+
+export default config;
